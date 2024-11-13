c60c778141f7cce39dd882e2b7cf06ebf51e2d06
Germano Ventura <og3rm4n0@gmail.com>
2024-11-13 20:57:54 -0300

    Add files via upload

---
+++ Exemplo.java
@@ -0,0 +1,11 @@
+public class Exemplo 
+{
+    public static void main(String[] args) {
+        int numero = 5;
+        String texto = "10";
+        
+        int resultado = numero + Integer.parseInt(texto);
+        
+        System.out.println("O resultado é: " + resultado);
+    }
+}
