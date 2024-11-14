2c29d19a06aef63217841e7eba11166b92485f40
Germano Ventura <og3rm4n0@gmail.com>
2024-11-14 19:49:12 -0300

    Add files via upload

---
+++ ExemploWhile.java
@@ -0,0 +1,10 @@
+//while é utilizado quando o nº de repetições não é conhecido previamente 
+public class ExemploWhile {
+    public static void main (String[] args){
+        int i = 1;
+        while (i <=5) {
+            System.out.println("Número: "+ i);
+            i++;
+        }
+    }
+}
