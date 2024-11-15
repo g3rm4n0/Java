d6f856ea81febd72ade0c84b58f90631b112b774
Germano Ventura <og3rm4n0@gmail.com>
2024-11-14 21:04:28 -0300

    Add files via upload

---
+++ ExemploIfElse.java
@@ -0,0 +1,12 @@
+public class ExemploIfElse {
+    public static void main (String[] args){
+        int anosDeExperiencia = 5;
+        if(anosDeExperiencia <2) {
+            System.out.println("Você é um desenvolvedor júnior!");
+        } else if (anosDeExperiencia >= 2 && anosDeExperiencia <5) {
+            System.out.println("Você é um desenvolvedor pleno!");
+        } else {
+            System.out.println("Você é um desenvolvedor sênior!");
+        }
+    }
+}
